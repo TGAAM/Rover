c342ca6926e310fbe70b488c28545cc716fcaa72
TGAAM <bassartemix@gmail.com>
2020-05-01 07:03:49 -0400

    changed to commands

--- rover.py
+++ rover.py
@@ -7,7 +7,9 @@ discord.py==1.3.3
 import discord
 import os
 import random
-
+from discord.ext import commands
+from discord.ext.commands import CommandNotFound
+from discord.ext.commands import CheckFailure
 
 # live channel whitelist
 # rovers-park
@@ -18,41 +20,92 @@ whitelistChannelsLive = [705571242312335431]
 whitelistChannelsTest = [697060204571000903, 698135978270916678, 698135987225886740, 705578025085042729]
 
 
-# setup a discord client
-client = discord.Client()
+# setup a discord client and bot commands
+#client = discord.Client()
+bot = commands.Bot(command_prefix="%")
 
+# don't work in DMs
+@bot.check
+async def globally_block_dms(ctx):
+    return ctx.guild is not None
+
+async def is_live_room(ctx):
+    return (ctx.channel.id in whitelistChannelsLive or ctx.channel.id in whitelistChannelsTest)
+
+async def is_test_room(ctx):
+    return (ctx.channel.id in whitelistChannelsTest)
+
+
+
+
+
+"""
 # initial setup 
 @client.event
 async def on_ready():
     # notify the log that it's ready
     print('We have logged in as {0.user}'.format(client))
+"""
+
+"""
+Live commmands
+"""
+
+@bot.command()
+@commands.check(is_live_room)
+async def hi(ctx):
+    await ctx.send("Hey there buddy")
+    return
+
+@bot.command()
+@commands.check(is_live_room)
+async def beep(ctx):
+    await ctx.send("Boop!")
+    return
+
+@bot.command()
+@commands.check(is_live_room)
+async def join(ctx):
+    await ctx.send(".john")
+    return
+
+@bot.command()
+@commands.check(is_live_room)
+async def champion(ctx):
+    await ctx.send("https://www.youtube.com/watch?v=6ICTd__N1OY")
+    return
+
+@bot.command()
+@commands.check(is_live_room)
+async def fishing(ctx):
+    await ctx.send("You caught a sea bass! \n I'd give it a C+!")
+    return
 
 
-# message posted
-# command identifier is %
-@client.event
-async def on_message(message):
+"""
+Test only commands
+"""
+@bot.command()
+@commands.check(is_test_room)
+async def test(ctx):
+    await ctx.send("This is a test only command")
+    return
 
-    # don't respond to itself
-    if message.author == client.user:
+
+
+# error handling
+@bot.event
+async def on_command_error(ctx, error):
+    # ignore errors that are invalid commands or calls from failed checks for now
+    if isinstance(error, CommandNotFound):
+        return
+    elif isinstance(error, CheckFailure):
         return
 
-    # live commands
-    if (message.channel.id in whitelistChannelsLive or message.channel.id in whitelistChannelsTest):
-
-        # simple answer/reply command
-        if message.content.startswith("%beep"):
-            await message.channel.send('Boop!')
-            return
+    raise error
 
 
-    # test only commands
-    if (message.channel.id in whitelistChannelsTest):
-        
-        # champion lance theme
-        if message.content.startswith("%champion"):
-            await message.channel.send("https://www.youtube.com/watch?v=6ICTd__N1OY")
 
 # start the program
 # I had to manually add the bot token to Heroku's config variables
-client.run(os.environ['ROVER_TOKEN'])
+bot.run(os.environ['ROVER_TOKEN'])
