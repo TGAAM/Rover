b5a7c480d055d144068e06577166e3f98019816a
TGAAM <bassartemix@gmail.com>
2020-07-11 13:02:51 -0400

    Testing date_announce

--- rover.py
+++ rover.py
@@ -4,14 +4,14 @@
 discord.py==1.3.3
 '''
 
-import discord
-import os
-import random
-from discord.ext import commands
+import discord, os, random, asyncio, traceback, json
+from discord.ext import commands, tasks
 from discord.ext.commands import CommandNotFound
 from discord.ext.commands import CheckFailure
 from discord.ext.commands import MissingRequiredArgument
+from datetime import datetime, timedelta
 
+calendar_update_send_time = '13:03'
 
 # live channel whitelist
 # rovers-park, #the-plaza, #the-roost
@@ -373,6 +373,54 @@ async def peel(ctx):
     await ctx.send("https://www.youtube.com/watch?v=4yHijxLoAPA")
     return
 
+async def timed_jobs():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        now = datetime.strftime(datetime.now(), '%H:%M') 
+
+        # calendar events
+        if now == calendar_update_send_time:
+            guild = bot.get_guild(689079080892760125)
+            channel = guild.get_channel(697060204571000903)
+
+            send_date = datetime.strftime(datetime.now() + timedelta(days = 1), '%B-%d') 
+            await post_events(channel, send_date)
+            time = 90
+        else:
+            time = 30
+        await asyncio.sleep(time)
+
+
+@bot.command()
+@commands.check(is_test_room)
+async def today(ctx):
+    now = datetime.strftime(datetime.now(), '%B-%d') 
+    await post_events(ctx.channel, now)
+    return
+
+@bot.command()
+@commands.check(is_test_room)
+async def tomorrow(ctx):
+    now = datetime.strftime(datetime.now() + timedelta(days = 1), '%B-%d') 
+    await post_events(ctx.channel, now)
+    return
+
+
+@bot.command()
+@commands.check(is_test_room)
+async def date(ctx, now):
+    await post_events(ctx.channel, now)
+    return
+
+async def post_events(channel, now):
+    with open('resources/ac_events.json') as f:
+                data = json.load(f)
+
+    date_details = data[now]
+    embed=discord.Embed(title="Event plans for : " + now, description=date_details["event"], color=0xffc572)
+    await channel.send(embed=embed)
+    return
+
 
 """
 Test only commands
@@ -419,5 +467,5 @@ async def on_command_error(ctx, error):
 
 
 # start the program
-# I had to manually add the bot token to Heroku's config variables
+bot.loop.create_task(timed_jobs())
 bot.run(os.environ['ROVER_TOKEN'])
